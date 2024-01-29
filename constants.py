63e483294630600d935328c315b531403880b044
MartinBoeckling <m_boeck@outlook.de>
2024-01-29 15:20:18 +0100

    Delete old constants file

--- constants.py
+++
@@ -1,9 +0,0 @@
-"""
-File containing the different constants relevant for this repository
-"""
-# data gathering variables
-helper_path_directory = "/ceph/mboeckli/stkg/helper"
-osm_data_path = "/ceph/mboeckli/stkg_comparison_data/airpolution_data/base_data/openstreetmap/seoul"
-osm_start_date = "2017-01-01"
-osm_end_date = "2020-01-01"
-osm_area = "Seoul, South Korea"
